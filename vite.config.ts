b87dbb7fd19940e16eae2b0662c7cf5363f15768
An Jiongyi <anjiongyi@163.com>
2024-09-13 10:15:38 +0800

    feat: use env. deps: add pwa support.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
