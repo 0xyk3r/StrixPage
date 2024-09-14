086616ad3a71455fe9afd2a9115338fa2d635b11
An Jiongyi <anjiongyi@163.com>
2024-09-14 22:05:58 +0800

    feat: Server Worker Auto Check Update.

--- src/components/ReloadPrompt.vue
+++ src/components/ReloadPrompt.vue
@@ -3,7 +3,30 @@ import { NButton, useNotification } from 'naive-ui'
 import { useRegisterSW } from 'virtual:pwa-register/vue'
 import { h, watch } from 'vue'
 
-const { offlineReady, needRefresh, updateServiceWorker } = useRegisterSW()
+const { offlineReady, needRefresh, updateServiceWorker } = useRegisterSW({
+  onRegisteredSW(swUrl, r) {
+    // 每小时检查一次更新
+    r &&
+      setInterval(
+        async () => {
+          if (r.installing || !navigator) return
+
+          if ('connection' in navigator && !navigator.onLine) return
+
+          const resp = await fetch(swUrl, {
+            cache: 'no-store',
+            headers: {
+              cache: 'no-store',
+              'cache-control': 'no-cache'
+            }
+          })
+
+          if (resp?.status === 200) await r.update()
+        },
+        60 * 60 * 1000
+      )
+  }
+})
 const notification = useNotification()
 
 watch(offlineReady, (newVal) => {
@@ -11,7 +34,6 @@ watch(offlineReady, (newVal) => {
     const offlineNotify = notification.warning({
       title: 'Strix 提示',
       content: '网络异常, Strix 目前处于离线模式.',
-      duration: 0,
       action: () =>
         h(
           NButton,
@@ -33,7 +55,6 @@ watch(needRefresh, (newVal) => {
     const updateNotify = notification.warning({
       title: 'Strix 提示',
       content: 'Strix 已更新, 点击确认切换为最新版本.',
-      duration: 0,
       meta: '请注意保存您的内容',
       action: () =>
         h(
