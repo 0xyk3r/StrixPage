f39d56bf544086617246756be7c271776ccf4b45
0xyk3r <anjiongyi@163.com>
2025-02-21 12:31:32 +0800

    feat: Strix 工作流增加抄送对象选择.

--- src/components/workflow/props/CcProps.vue
+++ src/components/workflow/props/CcProps.vue
@@ -1,37 +1,53 @@
 <template>
-  <n-form :model="cacheProps">
-    <n-form-item label="抄送对象类型">
-      <n-radio-group v-model:value="cacheProps.assign.type">
-        <n-flex>
-          <n-radio v-for="item in assignTypes" :key="item.value" :value="item.value">
-            {{ item.label }}
-          </n-radio>
-        </n-flex>
-      </n-radio-group>
-    </n-form-item>
-    <n-form-item
-      v-if="cacheProps.assign.type === 'USER' || cacheProps.assign.type === 'ROLE'"
-      label="抄送对象选择"
-    >
-      <n-button v-if="cacheProps.assign.type === 'USER'" type="primary" @click="openSelectUser">
-        选择人员
-      </n-button>
-      <n-button v-if="cacheProps.assign.type === 'ROLE'" type="primary" @click="openSelectRole">
-        选择角色
-      </n-button>
-    </n-form-item>
-    <n-form-item label="允许发起人添加抄送对象">
-      <n-radio-group v-model:value="cacheProps.allowAdd">
-        <n-flex>
-          <n-radio v-for="(item, index) in ccAllowAddItems" :key="index" :value="item.value">
-            {{ item.label }}
-          </n-radio>
-        </n-flex>
-      </n-radio-group>
-    </n-form-item>
-  </n-form>
+  <div>
+    <n-form :model="cacheProps">
+      <n-form-item label="抄送对象类型">
+        <n-radio-group v-model:value="cacheProps.assign.type">
+          <n-flex>
+            <n-radio v-for="item in assignTypes" :key="item.value" :value="item.value">
+              {{ item.label }}
+            </n-radio>
+          </n-flex>
+        </n-radio-group>
+      </n-form-item>
+      <n-form-item
+        v-if="cacheProps.assign.type === 'USER' || cacheProps.assign.type === 'ROLE'"
+        label="抄送对象选择"
+      >
+        <n-button v-if="cacheProps.assign.type === 'USER'" type="primary" @click="openUserSelector">
+          选择人员
+        </n-button>
+        <n-button v-if="cacheProps.assign.type === 'ROLE'" type="primary" @click="openRoleSelector">
+          选择角色
+        </n-button>
+      </n-form-item>
+      <n-form-item label="允许发起人添加抄送对象">
+        <n-radio-group v-model:value="cacheProps.allowAdd">
+          <n-flex>
+            <n-radio v-for="(item, index) in ccAllowAddItems" :key="index" :value="item.value">
+              {{ item.label }}
+            </n-radio>
+          </n-flex>
+        </n-radio-group>
+      </n-form-item>
+    </n-form>
+    <system-manager-selector
+      v-model:show="showUserSelector"
+      :value="cacheProps.assign.id"
+      @complete="handleSelectUserComplete"
+    />
+    <system-role-selector
+      v-model:show="showRoleSelector"
+      :value="cacheProps.assign.id"
+      @complete="handleSelectRoleComplete"
+    />
+    {{ cacheProps }}
+  </div>
 </template>
 <script lang="ts" setup>
+import SystemManagerSelector from '@/components/common/SystemManagerSelector.vue'
+import SystemRoleSelector from '@/components/common/SystemRoleSelector.vue'
+
 const $props = defineProps({
   modelValue: {
     type: Object,
@@ -51,11 +67,33 @@ const ccAllowAddItems = [
   { label: '允许', value: true },
   { label: '不允许', value: false }
 ]
-const openSelectUser = () => {
-  console.log('openSelectUser')
+
+// 监听抄送对象类型变化 清空选择的人员或角色
+watch(
+  () => cacheProps.value.assign.type,
+  () => {
+    cacheProps.value.assign.id = []
+  }
+)
+
+// 选择人员
+const showUserSelector = ref(false)
+const openUserSelector = () => {
+  showUserSelector.value = true
 }
-const openSelectRole = () => {
-  console.log('openSelectRole')
+const handleSelectUserComplete = (data: Array<string | number>) => {
+  cacheProps.value.assign.id = data
+  showUserSelector.value = false
+}
+
+// 选择角色
+const showRoleSelector = ref(false)
+const openRoleSelector = () => {
+  showRoleSelector.value = true
+}
+const handleSelectRoleComplete = (data: Array<string | number>) => {
+  cacheProps.value.assign.id = data
+  showRoleSelector.value = false
 }
 </script>
 <style lang="scss" scoped></style>
