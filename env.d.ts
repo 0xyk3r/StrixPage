8bd4fbc5828e7000ec79bd8749a4d622c040324d
0xyk3r <anjiongyi@163.com>
2026-04-02 05:21:06 +0800

    fix: 修复问题.

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,1 @@
 /// <reference types="vite/client" />
-
-declare module 'vue' {
-  interface GlobalComponents {
-    StrixEmpty: (typeof import('./src/components/common/StrixEmpty.vue'))['default']
-  }
-}
